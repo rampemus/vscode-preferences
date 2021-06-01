4427d86f9979cc9c9918c24de835a186b9f6bdca
rampemus <pasi.toivanen@live.com>
2021-06-01 22:16:13 +0300

    Touch added as a comment

--- nvimsync.sh
+++ nvimsync.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+###
+# mkdir ~/.config
+# touch ~/.config/nvim/init.vim
+
 cp ./config/init.vim ~/.config/nvim/init.vim
 cp -rf ./config/autoload ~/.config/nvim/
 cp -rf ./config/colors ~/.config/nvim/
