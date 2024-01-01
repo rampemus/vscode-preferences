5121e70e12471181abe8d7f129b5f3713e537e40
rampemus <pasi.toivanen@live.com>
2024-01-01 15:04:58 +0200

    Autopair, move leader h to leader g

--- config/init.lua
+++ config/init.lua
@@ -185,14 +185,14 @@ require('lazy').setup({
         map('n', 'ghb', function()
           gs.blame_line { full = false }
         end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
+        map('n', '<leader>gd', gs.diffthis, { desc = 'git diff against index' })
+        map('n', '<leader>gD', function()
           gs.diffthis '~'
         end, { desc = 'git diff against last commit' })
 
         -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+        map('n', '<leader>gb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
+        map('n', '<leader>gd', gs.toggle_deleted, { desc = 'toggle git show deleted' })
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
@@ -246,6 +246,15 @@ require('lazy').setup({
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
+      },
+    },
+  },
+
+  {
+    'altermo/ultimate-autopair.nvim',
+    opts = {
+      cr = {
+        enable = false,
       },
     },
   },
