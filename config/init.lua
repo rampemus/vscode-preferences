b78653ce19563cf7de393f8e239eed533949616d
rampemus <pasi.toivanen@live.com>
2024-01-20 20:12:35 +0200

    Show filename first in telescope results

--- config/init.lua
+++ config/init.lua
@@ -592,6 +592,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+local function filenameFirst(_, path)
+  local tail = vim.fs.basename(path)
+  local parent = vim.fs.dirname(path)
+  if parent == "." then return tail end
+  return string.format("%s\t\t%s", tail, parent)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopeResults",
+  callback = function(ctx)
+    vim.api.nvim_buf_call(ctx.buf, function()
+      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+    end)
+  end,
+})
+
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -605,6 +622,7 @@ require('telescope').setup {
     layout_config = {
       prompt_position = 'top',
     },
+    path_display = filenameFirst,
   },
 }
 
