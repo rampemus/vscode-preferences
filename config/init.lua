8ed2d0b9034b8a81d3ba181291c3f2ccbfddb255
rampemus <pasi.toivanen@live.com>
2024-04-04 23:26:55 +0300

    Use gj in visual

--- config/init.lua
+++ config/init.lua
@@ -580,6 +580,8 @@ vim.cmd([[
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('v', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('v', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
 if vim.g.started_by_firenvim then
