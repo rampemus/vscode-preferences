1810be4cdaafe418b11ba3156ab1ad14b9fc2e8f
rampemus <pasi.toivanen@live.com>
2024-05-01 23:17:55 +0300

    Add missing files to remove and fix udpate script

--- nvimremove.sh
+++ nvimremove.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 rm ~/.vimrc
+
+# Remove vim-plug
 rm -rf ~/.vim/autoload
 
+rm ~/.config/nvim/common.vim
+rm ~/.config/nvim/firenvim.vim
 rm ~/.config/nvim/firenvim.vim
 rm ~/.config/nvim/init.vim
+rm ~/.config/nvim/lazy-lock.json
 rm ~/.config/nvim/lua.vim
+rm ~/.config/nvim/startup.vim
+rm ~/.config/nvim/util.vim
 rm ~/.config/nvim/vscode.vim
+
+rm -rf ~/.config/coc
+rm -rf ~/.config/nvim-plugins
 rm -rf ~/.config/nvim/autoload
 rm -rf ~/.config/nvim/colors
 rm -rf ~/.config/nvim/pack
-rm -rf ~/.config/nvim-plugins
-rm -rf ~/.config/coc
+rm -rf ~/.config/nvim/snippets
 rm -rf ~/.local/share/nvim
--- nvimupdate.sh
+++ nvimupdate.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 vim +'PlugUpdate' +qa
-nvim +'PlugUpdate' +qa
 
-nvim +'PlugSnapshot config/version-lock.vim' +qa
+# Update neovim plug dependencies
+rm ~/.config/nvim/init.lua
+cp ./config/init.vim ~/.config/nvim/init.vim
+nvim +'PlugUpdate' +qa
+# Update plug lock file
+nvim +'PlugSnapshot! config/version-lock.vim' +qa
+# Put the init.lua back and update lazy plugins
+rm ~/.config/nvim/init.vim
+cp ./config/init.lua ~/.config/nvim/init.lua
+nvim --headless "+Lazy! update" +qa
+
+# Update lazy lock file
+cp -f ~/.config/nvim/lazy-lock.json ./config/lazy-lock.json
