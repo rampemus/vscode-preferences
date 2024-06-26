5d6e6c001d0ec532446480fe41757dede7073b2b
rampemus <pasi.toivanen@live.com>
2024-06-26 22:03:27 +0300

    Change colorizer to nvchad fork

--- config/init.lua
+++ config/init.lua
@@ -305,9 +305,27 @@ require('lazy').setup({
     },
   },
   {
-    'brenoprata10/nvim-highlight-colors',
+    'NvChad/nvim-colorizer.lua',
     event = 'VeryLazy',
-    opts = {},
+    config = function()
+      require('colorizer').setup({
+        filetypes = { "*" },
+        user_default_options = {
+          RGB = true,
+          RRGGBB = true,
+          names = true,
+          RRGGBBAA = true,
+          AARRGGBB = true,
+          rgb_fn = true,
+          hsl_fn = true,
+          css = true,
+          css_fn = true,
+          mode = "background",
+          sass = { enable = false, parsers = { "css" }, },
+          virtualtext = "?",
+        },
+      })
+    end,
   },
   {
     'lukas-reineke/virt-column.nvim',
