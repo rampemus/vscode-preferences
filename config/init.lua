fd9729376f62236d52fafab550f6530494bc90f3
rampemus <pasi.toivanen@live.com>
2024-06-29 21:25:56 +0300

    Revert cmp c-p

--- config/init.lua
+++ config/init.lua
@@ -1021,6 +1021,8 @@ cmp.setup({
     completeopt = 'menu,menuone,noinsert',
   },
   mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
