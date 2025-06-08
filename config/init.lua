f1cfeb6f95a3eee4e01b49f001ccbdc5265be377
rampemus <pasi.toivanen@live.com>
2025-06-08 12:03:57 +0300

    Use opts

--- config/init.lua
+++ config/init.lua
@@ -393,25 +393,23 @@ require('lazy').setup({
   {
     'NvChad/nvim-colorizer.lua',
     event = 'VeryLazy',
-    config = function()
-      require('colorizer').setup({
-        filetypes = { '*' },
-        user_default_options = {
-          RGB = true,
-          RRGGBB = true,
-          names = true,
-          RRGGBBAA = true,
-          AARRGGBB = true,
-          rgb_fn = true,
-          hsl_fn = true,
-          css = true,
-          css_fn = true,
-          mode = 'background',
-          sass = { enable = false, parsers = { 'css' }, },
-          virtualtext = '?',
-        },
-      })
-    end,
+    opts = {
+      filetypes = { '*' },
+      user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+        mode = 'background',
+        sass = { enable = false, parsers = { 'css' }, },
+        virtualtext = '?',
+      },
+    },
   },
   {
     'lukas-reineke/virt-column.nvim',
