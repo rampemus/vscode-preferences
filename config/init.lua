e24881e878e34cfb0e0bc0f477e0e721430e9a4b
rampemus <pasi.toivanen@live.com>
2025-10-21 22:59:58 +0300

    Fallback to lsp if prettier does not work

--- config/init.lua
+++ config/init.lua
@@ -1022,6 +1022,14 @@ vim.api.nvim_create_user_command('TelescopeFindFiles', function()
     Telescope find_files --sort=modified\n
   ]])
 end, {})
+vim.api.nvim_create_user_command('PrettierOrFormat', function()
+  vim.cmd([[
+    silent! Prettier
+  ]])
+  if vim.v.shell_error ~= 0 then
+    vim.lsp.buf.format()
+  end
+end, {})
 
 -- Telescope live_grep in git root
 -- Function to find the git root directory based on the current buffer's path
@@ -1151,24 +1159,6 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
   nmap('<leader>f', vim.lsp.buf.format, '[F]ormat')
 end
-
--- document existing key chains
-require('which-key').register({
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-})
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
