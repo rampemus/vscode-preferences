54f4e5197daf52645cf6875e05a4a1ce376822c0
rampemus <pasi.toivanen@live.com>
2025-10-26 23:27:10 +0200

    No comments

--- config/init.lua
+++ config/init.lua
@@ -768,24 +768,6 @@ require('lazy').setup({
   },
   'nvim-tree/nvim-web-devicons',
 
-  -- {
-  --   "Equilibris/nx.nvim",
-  --   event = 'VeryLazy',
-  --   enabled = not vim.g.started_by_firenvim,
-  --   dependencies = {
-  --     "nvim-telescope/telescope.nvim",
-  --   },
-  --   opts = {
-  --     nx_cmd_root = "npx nx",
-  --   },
-  --   keys = {
-  --     { "<leader>na", "<cmd>Telescope nx actions<CR>", desc = "nx actions" },
-  --     { "<leader>ng", "<cmd>Telescope nx generators<CR>", desc = "nx generators" },
-  --     { "<leader>neg", "<cmd>Telescope nx external_generators<CR>", desc = "nx external generators" },
-  --     { "<leader>nr", "<cmd>Telescope nx run_many<CR>", desc = "nx run_many" },
-  --   },
-  -- },
-
   ---@diagnostic disable-next-line: missing-fields
 }, {})
 
@@ -1079,21 +1061,6 @@ vim.keymap.set('n', '<leader>ö', function()
   }))
 end, { desc = '[/] Fuzzily search in current buffer' })
 
--- local function telescope_live_grep_open_files()
---   require('telescope.builtin').live_grep({
---     grep_open_files = true,
---     prompt_title = 'Live Grep in Open Files',
---   })
--- end
--- vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
--- vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
--- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
--- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
--- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
--- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
--- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
--- vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
--- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- [[ Configure Treesitter ]]
