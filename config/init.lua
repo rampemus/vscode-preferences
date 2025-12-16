75de5188fc77fdbef9b3aea54eba58d4c572b9bf
Pasi Toivanen <pasi.toivanen@fmi.fi>
2025-12-16 15:34:19 +0200

    Use nerd font

--- config/init.lua
+++ config/init.lua
@@ -165,7 +165,7 @@ require('lazy').setup({
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
         underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = vim.g.have_nerd_font and {
+        signs = {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
             [vim.diagnostic.severity.WARN] = '󰀪 ',
