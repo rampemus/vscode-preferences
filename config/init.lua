d49199afae377da39d726785d25407ba8c5414e7
Pasi Toivanen <pasi.toivanen@fmi.fi>
2026-02-19 13:22:15 +0200

    Add mail search shortcut and nx plugin

--- config/init.lua
+++ config/init.lua
@@ -955,6 +955,23 @@ require('lazy').setup({
     end,
   },
 
+  {
+    "Equilibris/nx.nvim",
+
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+
+    opts  = {
+      -- See below for config options
+      nx_cmd_root = "npx nx",
+    },
+
+    -- Plugin will load when you use these keys
+    keys = {
+      { "<leader>nx", "<cmd>Telescope nx actions<CR>", desc = "nx actions"}
+    },
+  },
 }, {
   change_detection = { notify = false },
   rocks = {
